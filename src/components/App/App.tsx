d3a8bc7e9c90a66fc6d92036f9586289ada9c115
carloslara33 <38373196+carloslara33@users.noreply.github.com>
2024-04-11 13:14:34 +0200

    App wrapper and navbar created.
    I have created the wrapper component for the app; this will wrap all the routes of the app. I have also added some components like the chart and the navbar.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,100 @@
-import React from "react";
-import "./App.css";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-export const App = () => {
+import Box from "@mui/material/Box";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "../Drawer/Drawer.tsx";
+
+const DRAWERWIDTH: number = 260;
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: DRAWERWIDTH,
+    width: `calc(100% - ${DRAWERWIDTH}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function App() {
+  const [isOpen, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={isOpen}>
+        <Toolbar
+          sx={{
+            pr: "24px", 
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              ...(isOpen && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        drawerWitdh={DRAWERWIDTH}
+        isOpen={isOpen}
+        toggleDrawer={toggleDrawer}
+      />
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) => theme.palette.grey[100],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+ 
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
   );
-};
+}
---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import MuiDrawer from "@mui/material/Drawer";
+import { styled } from "@mui/material/styles";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+
+interface DrawerProps {
+  drawerWitdh: number;
+  isOpen: boolean;
+  toggleDrawer: () => void;
+}
+
+const StyledMuiDrawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open, width }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: width,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+export default function Dashboard({
+  drawerWitdh,
+  isOpen,
+  toggleDrawer,
+}: DrawerProps) {
+  return (
+    <StyledMuiDrawer variant="permanent" open={isOpen} width={drawerWitdh}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <IconButton onClick={toggleDrawer}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <List component="nav">
+        <React.Fragment>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Solar" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Gas" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Hydro" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Kinetic" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Thermal" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Real time information" />
+          </ListItemButton>
+        </React.Fragment>
+      </List>
+    </StyledMuiDrawer>
+  );
+}
