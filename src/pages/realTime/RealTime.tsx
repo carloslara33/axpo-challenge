850782d39efb152ecb32ba72b0df7b5a465838d4
carloslara33 <38373196+carloslara33@users.noreply.github.com>
2024-04-22 13:55:04 +0200

    Added agGrid as reactive table

--- src/pages/realTime/RealTime.tsx
+++ src/pages/realTime/RealTime.tsx
@@ -1,9 +1,36 @@
 import React, { useContext } from "react";
+import { AgGridReact } from "ag-grid-react"; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
 import { OrdersContext } from "../../contexts/OrdersContext.tsx";
+
+const columnDefs = [
+  { field: "id" },
+  { field: "orderType" },
+  { field: "status" },
+  { field: "capacity" },
+  { field: "location" },
+  { field: "outputPrediction" },
+  {
+    field: "certifications",
+    valueFormatter: (params) => params.value?.join(", "),
+  },
+  { field: "price" },
+  { field: "minQuantity" },
+  { field: "contractTerms" },
+  { field: "paymentTerms" },
+];
 
 export default function RealTime() {
   const { orders } = useContext(OrdersContext);
 
-  return <div>{orders ? JSON.stringify(orders) : "Loading...."}</div>;
+  return (
+    <div
+      className="ag-theme-quartz" 
+      style={{ height: 500 }} 
+    >
+      <AgGridReact rowData={orders} columnDefs={columnDefs} />
+    </div>
+  );
 }
